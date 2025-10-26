91666e35032d11912a32dd7faf6ab318532c273f
AmritM05 <mareddy.2@buckeyemail.osu.edu>
2025-10-25 21:17:40 -0400

    backend + small frontend changes

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -5,14 +5,19 @@ import cors from "cors";
 import path from "path";
 import { fileURLToPath } from "url";
 
+
 dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 // Resolve __dirname for ES modules
+import path from "path";
+import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+app.use(express.static(__dirname));
+
 
 // ✅ Serve your static files (index.html, tracker.html, summary.html, style.css, tracker.js, summary.js)
 app.use(express.static(__dirname));
